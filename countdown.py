f92be7e743f7af48ec867569128b6835f709a6ca
nirmmalesh <darkestday0809@gmail.com>
2026-01-30 13:50:35 +0800

    Add files via upload

---
+++ countdown.py
@@ -0,0 +1,11 @@
+import time
+
+my_time = int(input("set the timer countdown in seconds: "))
+for counter in range(my_time, 0, -1):
+    seconds = counter % 60
+    minutes = int(counter / 60) % 60
+    hours = int(counter /3600)
+    print(f"{hours}:{minutes}:{seconds}")
+    time.sleep(1)
+
+print("TIME'S UP!")
