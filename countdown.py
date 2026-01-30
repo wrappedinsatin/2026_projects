f041ef686eabdc0e9ebc9d6e26d45988145d486c
nirmmalesh <darkestday0809@gmail.com>
2026-01-30 13:52:18 +0800

    Add files via upload

--- countdown.py
+++ countdown.py
@@ -5,7 +5,7 @@ for counter in range(my_time, 0, -1):
     seconds = counter % 60
     minutes = int(counter / 60) % 60
     hours = int(counter /3600)
-    print(f"{hours}:{minutes}:{seconds}")
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
     time.sleep(1)
 
 print("TIME'S UP!")
