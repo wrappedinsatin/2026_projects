0472e7381375fb4ed65c88ccd91e85b3af76a838
kugelblitzes <darkestday0809@gmail.com>
2026-02-27 11:31:14 +0800

    Add files via upload

--- story.py
+++ story.py
@@ -1,16 +1,14 @@
 story_list = []
 
 while True:
+
     next_word = input("Please type in a word: ")
 
     if next_word == "end":
         break
-    elif len(story_list) < 1:
+    
+    if len(story_list) == 0 or story_list[-1] != next_word:
         story_list.append(next_word)
-    else:
-        for i in range(1, len(story_list)):
-            if story_list[i] != story_list[i + 1]:
-                story_list.append(next_word)
 
 story_string = ' '.join(story_list)
 print(story_string)
