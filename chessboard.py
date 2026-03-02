5a73846b51506b37f961ebcf27f9781d4935edf7
kugelblitzes <darkestday0809@gmail.com>
2026-03-02 12:32:12 +0800

    Add files via upload
    chessboard program for mooc helsinki

---
+++ chessboard.py
@@ -0,0 +1,30 @@
+
+def ranks(num):
+    
+    rank = ""
+    square = 1
+
+    if len(rank) == num and rank[0] == "1":
+        rank = ""
+        square = 0
+
+    while not len(rank) > num:
+        rank += str(square)
+        square -= 1
+
+        if square < 0:
+            square = 1
+
+        if len(rank) == num:
+            print(rank)
+            break
+
+
+def chessboard(num):
+    rep = 0
+    while rep < num:
+        ranks(num)
+        rep += 1
+
+if __name__ == "__main__":
+    chessboard(num)
